5e03df33961e3942d133e638e1f3f218dd8daa85
ksinny <dosly2@naver.com>
2026-03-21 05:45:10 +0900

    feat: Service 및 Repository Bean 등록 및 테스트

--- 2-sprint-mission/src/main/java/com/sprint/mission/discodeit/DiscodeitApplication.java
+++ 2-sprint-mission/src/main/java/com/sprint/mission/discodeit/DiscodeitApplication.java
@@ -1,12 +1,31 @@
 package com.sprint.mission.discodeit;
 
+import com.sprint.mission.discodeit.entity.Channel;
+import com.sprint.mission.discodeit.entity.User;
+import com.sprint.mission.discodeit.service.ChannelService;
+import com.sprint.mission.discodeit.service.MessageService;
+import com.sprint.mission.discodeit.service.UserService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+
+import static com.sprint.mission.discodeit.JavaApplication.*;
 
 @SpringBootApplication
 public class DiscodeitApplication {
-
 	public static void main(String[] args) {
-		SpringApplication.run(DiscodeitApplication.class, args);
+		ConfigurableApplicationContext context = SpringApplication.run(DiscodeitApplication.class, args);
+
+		// 서비스 초기화
+		UserService userService = context.getBean(UserService.class);
+		ChannelService channelService = context.getBean(ChannelService.class);
+		MessageService messageService = context.getBean(MessageService.class);
+
+		// 셋업 및 테스트
+		User user = setupUser(userService);
+		Channel channel = setupChannel(channelService);
+		messageCreateTest(messageService, channel, user);
+
+
 	}
 }
