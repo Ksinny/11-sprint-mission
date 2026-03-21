b0bd074e6ad79b8df5124d5931a42b9627f775b7
ksinny <dosly2@naver.com>
2026-03-21 16:53:18 +0900

    feat: 신규 도메인 엔티티 추가

---
+++ 2-sprint-mission/src/main/java/com/sprint/mission/discodeit/entity/ReadStatus.java
@@ -0,0 +1,26 @@
+package com.sprint.mission.discodeit.entity;
+
+import lombok.Getter;
+
+import java.time.Instant;
+import java.util.UUID;
+
+@Getter
+public class ReadStatus extends BaseEntity {
+    private final UUID userId;
+    private final UUID channelId;
+    private Instant lastReadAt;
+
+    public ReadStatus(UUID userId, UUID channelId) {
+        super();
+        this.userId = userId;
+        this.channelId = channelId;
+        this.lastReadAt = Instant.now();
+    }
+
+    // updatedAt을 현재 시간으로 갱신
+    public void updateLastReadAt() {
+        this.lastReadAt = Instant.now();
+        super.timeUpdate();
+    }
+}
