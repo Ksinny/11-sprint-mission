5cd2e06dd79f9947b968ac47ff14e2e224211dd3
ksinny <dosly2@naver.com>
2026-03-22 22:08:58 +0900

    feat: User 수정 로직 구현
    - UpdateRequest DTO 생성
    - 수정 대상 ID와 DTO 파라미터 분리 설계
    - 프로필 이미지 선택적 업데이트

--- 2-sprint-mission/src/main/java/com/sprint/mission/discodeit/service/UserService.java
+++ 2-sprint-mission/src/main/java/com/sprint/mission/discodeit/service/UserService.java
@@ -1,7 +1,7 @@
 package com.sprint.mission.discodeit.service;
 
 import com.sprint.mission.discodeit.dto.UserDto;
-import com.sprint.mission.discodeit.entity.User;
+
 import java.util.List;
 import java.util.UUID;
 
@@ -9,6 +9,6 @@ public interface UserService {
     UserDto.Response create(UserDto.CreateRequest request);
     UserDto.Response findById(UUID id);
     List<UserDto.Response> findAll();
-    User update(UUID id, String userName, String nickname, String description, String email, String password, UUID profileImageId);
+    UserDto.Response update(UUID id, UserDto.UpdateRequest request);
     void delete(UUID id);
 }
