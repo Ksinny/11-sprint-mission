4dac1783f44564d0b632a2375932c2e4082217db
ksinny <dosly2@naver.com>
2026-03-23 02:24:27 +0900

    feat: 채널 수정 로직 구현

--- 2-sprint-mission/src/main/java/com/sprint/mission/discodeit/service/MessageService.java
+++ 2-sprint-mission/src/main/java/com/sprint/mission/discodeit/service/MessageService.java
@@ -1,13 +1,13 @@
 package com.sprint.mission.discodeit.service;
 
 import com.sprint.mission.discodeit.dto.MessageDto;
-import com.sprint.mission.discodeit.entity.Message;
+
 import java.util.List;
 import java.util.UUID;
 
 public interface MessageService {
     MessageDto.Response create(MessageDto.CreateRequest request);
     List<MessageDto.Response> findAllByChannelId(UUID channelId);
-    Message update(UUID id, String content);
+    MessageDto.Response update(UUID id, MessageDto.UpdateRequest request);
     void delete(UUID id);
 }
