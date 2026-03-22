e463b0d9d8947506f3446a9853c4307a7702df7a
ksinny <dosly2@naver.com>
2026-03-23 02:18:14 +0900

    feat: 특정 Channel의 메시지 목록 조회 기능 구현
    - findAll을 findAllByChannelId로 변경하여 채널별 필터링 적용

--- 2-sprint-mission/src/main/java/com/sprint/mission/discodeit/service/MessageService.java
+++ 2-sprint-mission/src/main/java/com/sprint/mission/discodeit/service/MessageService.java
@@ -7,8 +7,7 @@ import java.util.UUID;
 
 public interface MessageService {
     MessageDto.Response create(MessageDto.CreateRequest request);
-    Message findById(UUID id);
-    List<Message> findAll();
+    List<MessageDto.Response> findAllByChannelId(UUID channelId);
     Message update(UUID id, String content);
     void delete(UUID id);
 }
