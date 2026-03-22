ae85bf1d4846008bb9dcb2314cc052355f39c481
ksinny <dosly2@naver.com>
2026-03-23 01:14:14 +0900

    feat: Channel 수정 로직 구현
    - Channel 엔티티 update 메서드 최적화

--- 2-sprint-mission/src/main/java/com/sprint/mission/discodeit/service/ChannelService.java
+++ 2-sprint-mission/src/main/java/com/sprint/mission/discodeit/service/ChannelService.java
@@ -1,8 +1,6 @@
 package com.sprint.mission.discodeit.service;
 
 import com.sprint.mission.discodeit.dto.ChannelDto;
-import com.sprint.mission.discodeit.entity.Channel;
-import com.sprint.mission.discodeit.entity.ChannelType;
 
 import java.util.List;
 import java.util.UUID;
@@ -12,6 +10,6 @@ public interface ChannelService {
     ChannelDto.Response createPrivateChannel(ChannelDto.CreatePrivateRequest request);
     ChannelDto.Response findById(UUID id);
     List<ChannelDto.Response> findAllByUserId(UUID userId);
-    Channel update(UUID id, ChannelType type, String name, String description, List<UUID> memberIds);
+    ChannelDto.Response update(UUID id, ChannelDto.UpdateRequest request);
     void delete(UUID id);
 }
