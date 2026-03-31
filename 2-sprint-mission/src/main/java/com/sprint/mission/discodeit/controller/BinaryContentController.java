6c12190d838204813e3c0822e9139d7c2a00d165
Ksinny <dosly2@naver.com>
2026-03-31 22:46:28 +0900

    refactor: BinaryContent API RESTful 재설계
    - 메서드 반환 타입을 Entity에서 DTO로 변경

--- 2-sprint-mission/src/main/java/com/sprint/mission/discodeit/controller/BinaryContentController.java
+++ 2-sprint-mission/src/main/java/com/sprint/mission/discodeit/controller/BinaryContentController.java
@@ -1,10 +1,13 @@
 package com.sprint.mission.discodeit.controller;
 
+import com.sprint.mission.discodeit.dto.BinaryContentDto;
 import com.sprint.mission.discodeit.entity.BinaryContent;
 import com.sprint.mission.discodeit.service.BinaryContentService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -14,23 +17,28 @@ import java.util.List;
 import java.util.UUID;
 
 @RestController
-@RequestMapping("/api/binaryContent")
+@RequestMapping("/api/binaryContents")
 @RequiredArgsConstructor
 public class BinaryContentController {
 
-    private final BinaryContentService binaryContentService;
+  private final BinaryContentService binaryContentService;
 
-    @RequestMapping(value = "/find", method = RequestMethod.GET)
-    public ResponseEntity<BinaryContent> find(
-            @Valid @RequestParam("binaryContentId") UUID binaryContentId) {
-        BinaryContent binaryContent = binaryContentService.findById(binaryContentId);
-        return ResponseEntity.ok(binaryContent);
-    }
+  @GetMapping("/{binaryContentId}")
+  public ResponseEntity<BinaryContentDto.Response> find(
+      @PathVariable UUID binaryContentId) {
+    BinaryContent binaryContent = binaryContentService.findById(binaryContentId);
+    return ResponseEntity.ok(BinaryContentDto.Response.of(binaryContent));
+  }
 
-    @RequestMapping(value = "/findAllByIdIn", method = RequestMethod.GET)
-    public ResponseEntity<List<BinaryContent>> findAllByIdIn(
-            @Valid @RequestParam("binaryContentIds") List<UUID> binaryContentIds) {
-        List<BinaryContent> binaryContents = binaryContentService.findAllByIdIn(binaryContentIds);
-        return ResponseEntity.ok(binaryContents);
-    }
+  @GetMapping
+  public ResponseEntity<List<BinaryContentDto.Response>> findAllByIdIn(
+      @Valid @RequestParam List<UUID> binaryContentIds) {
+    List<BinaryContent> binaryContents = binaryContentService.findAllByIdIn(binaryContentIds);
+
+    List<BinaryContentDto.Response> responseList = binaryContents.stream()
+        .map(BinaryContentDto.Response::of)
+        .toList();
+    
+    return ResponseEntity.ok(responseList);
+  }
 }
