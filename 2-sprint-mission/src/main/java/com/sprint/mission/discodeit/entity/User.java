31412103b5f1718ca8fb91cf64fda9ae3320ee8d
Ksinny <dosly2@naver.com>
2026-03-31 21:31:02 +0900

    refactor: User API RESTful 재설계
    - API 스펙에 맞춰 Request/Response DTO 필드명 수정 및 최적화
    - 프론트엔드 연동을 위해 @RequestPart 파라미터명 스펙 일치화
    - UserStatusService의 updateByUserId 메서드에 DTO 파라미터 추가
    - update 로직 변경에 따른 UserStatus 엔티티 내 updateActiveTime 관련 로직 수정

--- 2-sprint-mission/src/main/java/com/sprint/mission/discodeit/entity/User.java
+++ 2-sprint-mission/src/main/java/com/sprint/mission/discodeit/entity/User.java
@@ -10,73 +10,73 @@ import java.util.UUID;
 @Getter
 @AllArgsConstructor
 public class User extends BaseEntity {
-    private String username;
-    private String nickname;
-    private String description;
-    private String email;
-    private String password;
-    private UUID profileImageId;
+
+  private String username;
+  private String nickname;
+  private String description;
+  private String email;
+  private String password;
+  private UUID profileImageId;
 
 
+  public void update(String newUsername, String newEmail, String newPassword) {
+    boolean anyValueUpdated = false;
 
-    public void update(String newUsername, String newNickname, String newDescription, String newEmail, String newPassword) {
-        boolean anyValueUpdated = false;
-
-        if (newUsername != null && !newUsername.equals(this.username)) {
-            this.username = newUsername;
-            anyValueUpdated = true;
-        }
-        if (newNickname != null && !newNickname.equals(this.nickname)) {
+    if (newUsername != null && !newUsername.equals(this.username)) {
+      this.username = newUsername;
+      anyValueUpdated = true;
+    }
+        /*if (newNickname != null && !newNickname.equals(this.nickname)) {
             this.nickname = newNickname;
             anyValueUpdated = true;
         }
         if (newDescription != null && !newDescription.equals(this.description)) {
             this.description = newDescription;
             anyValueUpdated = true;
-        }
-        if (newEmail != null && !newEmail.equals(this.email)) {
-            this.email = newEmail;
-            anyValueUpdated = true;
-        }
-        if (newPassword != null && !newPassword.equals(this.password)) {
-            this.password = newPassword;
-            anyValueUpdated = true;
-        }
-
-        if (anyValueUpdated) {
-            super.timeUpdate();
-        }
+        }*/
+    if (newEmail != null && !newEmail.equals(this.email)) {
+      this.email = newEmail;
+      anyValueUpdated = true;
+    }
+    if (newPassword != null && !newPassword.equals(this.password)) {
+      this.password = newPassword;
+      anyValueUpdated = true;
     }
 
-    // 프로필 이미지 수정
-    public void updateProfileImage(UUID newProfileImageId) {
-        if (newProfileImageId != null && !newProfileImageId.equals(this.profileImageId)) {
-            this.profileImageId = newProfileImageId;
-        }
-        super.timeUpdate();
+    if (anyValueUpdated) {
+      super.timeUpdate();
     }
+  }
 
-    // 비밀번호 검증
-    // 비밀번호 암호화는 Spring Security의 PasswordEncoder로 스프린트 미션에 맞추어 이후 진행 예정
-    public void validatePassword(String password) {
-        if (this.password == null || !this.password.equals(password)) {
-            throw new IllegalArgumentException("Invalid username or password");
-        }
+  // 프로필 이미지 수정
+  public void updateProfileImage(UUID newProfileImageId) {
+    if (newProfileImageId != null && !newProfileImageId.equals(this.profileImageId)) {
+      this.profileImageId = newProfileImageId;
     }
+    super.timeUpdate();
+  }
 
-    @Override
-    public String toString() {
-        return "사용자 [" +
-                "UUID: " + getId() +
-                "\n이름: " + getUsername() +
-                ", 별명: " + getNickname() +
-                ", 소개: " + getDescription() +
-                ", 이메일: " + getEmail() +
- //               ", 비밀번호: " + getPassword() + // 추후 비밀번호 관련 로직 변경 예정 + 제외
-                ", 프로필 사진: " + getProfileImageId() +
-                ", 생성 시간: " + getCreatedAt() +
-                ", 수정 시간: " + getUpdatedAt() +
-                "]\n" ;
+  // 비밀번호 검증
+  // 비밀번호 암호화는 Spring Security의 PasswordEncoder로 스프린트 미션에 맞추어 이후 진행 예정
+  public void validatePassword(String password) {
+    if (this.password == null || !this.password.equals(password)) {
+      throw new IllegalArgumentException("Invalid username or password");
     }
+  }
+
+  @Override
+  public String toString() {
+    return "사용자 [" +
+        "UUID: " + getId() +
+        "\n이름: " + getUsername() +
+        ", 별명: " + getNickname() +
+        ", 소개: " + getDescription() +
+        ", 이메일: " + getEmail() +
+        //               ", 비밀번호: " + getPassword() + // 추후 비밀번호 관련 로직 변경 예정 + 제외
+        ", 프로필 사진: " + getProfileImageId() +
+        ", 생성 시간: " + getCreatedAt() +
+        ", 수정 시간: " + getUpdatedAt() +
+        "]\n";
+  }
 
 }
--- 2-sprint-mission/src/main/java/com/sprint/mission/discodeit/service/basic/BasicUserService.java
+++ 2-sprint-mission/src/main/java/com/sprint/mission/discodeit/service/basic/BasicUserService.java
@@ -11,6 +11,7 @@ import com.sprint.mission.discodeit.repository.BinaryContentRepository;
 import com.sprint.mission.discodeit.repository.UserRepository;
 import com.sprint.mission.discodeit.repository.UserStatusRepository;
 import com.sprint.mission.discodeit.service.UserService;
+import java.util.Optional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -20,113 +21,109 @@ import java.util.UUID;
 @Service
 @RequiredArgsConstructor
 public class BasicUserService implements UserService {
-    private final UserRepository userRepository;
-    private final UserStatusRepository userStatusRepository;
-    private final BinaryContentRepository binaryContentRepository;
+
+  private final UserRepository userRepository;
+  private final UserStatusRepository userStatusRepository;
+  private final BinaryContentRepository binaryContentRepository;
 
 
-    @Override
-    public UserDto.Response create(UserDto.CreateRequest request, BinaryContentDto.CreateRequest profileImageRequest) {
-        // username 중복 확인
-        if (userRepository.existsByName(request.username())) {
-            throw new BusinessException(ErrorCode.DUPLICATE_NAME);
-        }
-
-        // email 중복 확인
-        if (userRepository.existsByEmail(request.email())) {
-            throw new BusinessException(ErrorCode.DUPLICATE_EMAIL);
-        }
-
-        UUID profileImageId = null;
-        if (profileImageRequest != null) {
-            BinaryContent binaryContent = profileImageRequest.toEntity();
-            BinaryContent savedContent = binaryContentRepository.save(binaryContent);
-            profileImageId = savedContent.getId();
-        }
-
-        User user = request.toEntity(profileImageId);
-        userRepository.save(user);
-
-        // UserStatus 함께 생성
-        UserStatus userStatus = new UserStatus(user.getId());
-        userStatusRepository.save(userStatus);
-
-        return UserDto.Response.of(user, userStatus);
+  @Override
+  public UserDto.Response create(UserDto.CreateRequest request,
+      BinaryContentDto.CreateRequest profileImageRequest) {
+    if (userRepository.existsByName(request.username())) {
+      throw new BusinessException(ErrorCode.DUPLICATE_NAME);
     }
 
-    @Override
-    public UserDto.Response findById(UUID id) {
-        User user = userRepository.findById(id)
-                .orElseThrow(() -> new BusinessException(ErrorCode.USER_NOT_FOUND));
-
-        UserStatus userStatus = userStatusRepository.findById(id)
-                .orElseThrow(() -> new BusinessException(ErrorCode.USER_STATUS_NOT_FOUND));
-
-        return UserDto.Response.of(user, userStatus);
+    if (userRepository.existsByEmail(request.email())) {
+      throw new BusinessException(ErrorCode.DUPLICATE_EMAIL);
     }
 
-    @Override
-    public List<UserDto.Response> findAll() {
-        List<User> users = userRepository.findAll();
-
-        return users.stream()
-                .map(user -> {
-                    UserStatus status = userStatusRepository.findByUserId(user.getId())
-                            .orElseGet(() -> UserStatus.builder()
-                                    .userId(user.getId())
-                                    .lastActiveAt(user.getCreatedAt())
-                                    .build());
-
-                    return UserDto.Response.of(user, status);
-                })
-                .toList();
+    UUID profileImageId = null;
+    if (profileImageRequest != null) {
+      BinaryContent binaryContent = profileImageRequest.toEntity();
+      BinaryContent savedContent = binaryContentRepository.save(binaryContent);
+      profileImageId = savedContent.getId();
     }
 
-    @Override
-    public UserDto.Response update(UUID id, UserDto.UpdateRequest request, BinaryContentDto.CreateRequest profileImageRequest) {
-        User user = userRepository.findById(id)
-                .orElseThrow(() -> new BusinessException(ErrorCode.USER_NOT_FOUND));
+    User user = request.toEntity(profileImageId);
+    userRepository.save(user);
 
-        // 프로필 이미지 변경 로직
-        if (profileImageRequest != null) {
-            // 기존 프로필 이미지 삭제
-            if (user.getProfileImageId() != null) {
-                binaryContentRepository.deleteById(user.getProfileImageId());
-            }
+    // UserStatus 함께 생성
+    UserStatus userStatus = new UserStatus(user.getId());
+    userStatusRepository.save(userStatus);
 
-            BinaryContent newImage = profileImageRequest.toEntity();
-            user.updateProfileImage(binaryContentRepository.save(newImage).getId());
-        }
+    return UserDto.Response.of(user, userStatus);
+  }
 
-        user.update(
-                request.username(),
-                request.nickname(),
-                request.description(),
-                request.email(),
-                request.password()
-        );
+  @Override
+  public UserDto.Response findById(UUID id) {
+    User user = userRepository.findById(id)
+        .orElseThrow(() -> new BusinessException(ErrorCode.USER_NOT_FOUND));
 
-        userRepository.save(user);
+    UserStatus userStatus = userStatusRepository.findById(id)
+        .orElseThrow(() -> new BusinessException(ErrorCode.USER_STATUS_NOT_FOUND));
 
-        // 상태 정보 반환
-        UserStatus status = userStatusRepository.findById(id)
-                .orElseGet(() -> new UserStatus(id));
+    return UserDto.Response.of(user, userStatus);
+  }
 
-        return UserDto.Response.of(user, status);
+  @Override
+  public List<UserDto.Response> findAll() {
+    List<User> users = userRepository.findAll();
+
+    return users.stream()
+        .map(user -> {
+          UserStatus status = userStatusRepository.findByUserId(user.getId())
+              .orElseGet(() -> UserStatus.builder()
+                  .userId(user.getId())
+                  .lastActiveAt(user.getCreatedAt())
+                  .build());
+
+          return UserDto.Response.of(user, status);
+        })
+        .toList();
+  }
+
+  @Override
+  public UserDto.Response update(UUID id, UserDto.UpdateRequest request,
+      BinaryContentDto.CreateRequest profileImageRequest) {
+    User user = userRepository.findById(id)
+        .orElseThrow(() -> new BusinessException(ErrorCode.USER_NOT_FOUND));
+
+    // 프로필 이미지 변경 로직
+    if (profileImageRequest != null) {
+      // 기존 프로필 이미지 삭제
+      if (user.getProfileImageId() != null) {
+        binaryContentRepository.deleteById(user.getProfileImageId());
+      }
+
+      BinaryContent newImage = profileImageRequest.toEntity();
+      user.updateProfileImage(binaryContentRepository.save(newImage).getId());
     }
 
-    @Override
-    public void delete(UUID id) {
-        User user = userRepository.findById(id)
-                .orElseThrow(() -> new BusinessException(ErrorCode.USER_NOT_FOUND));
+    user.update(
+        request.newUsername(),
+        request.newEmail(),
+        request.newPassword()
+    );
 
+    userRepository.save(user);
 
-        userStatusRepository.deleteById(id);
+    // 상태 정보 반환
+    UserStatus status = userStatusRepository.findById(id)
+        .orElseGet(() -> new UserStatus(id));
 
-        // 프로필 이미지 삭제 (존재 시)
-        if (user.getProfileImageId() != null) {
-            binaryContentRepository.deleteById(user.getProfileImageId());
-        }
-        userRepository.deleteById(id);
+    return UserDto.Response.of(user, status);
+  }
+
+  @Override
+  public void delete(UUID id) {
+    User user = userRepository.findById(id)
+        .orElseThrow(() -> new BusinessException(ErrorCode.USER_NOT_FOUND));
+
+    // 프로필 이미지 삭제 (존재 시)
+    if (user.getProfileImageId() != null) {
+      binaryContentRepository.deleteById(user.getProfileImageId());
     }
+    userRepository.deleteById(id);
+  }
 }
