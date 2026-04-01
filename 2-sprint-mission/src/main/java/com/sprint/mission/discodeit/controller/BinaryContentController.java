f2e5354f09e9e888bb9d9577fe9bb38586101f77
Ksinny <dosly2@naver.com>
2026-04-01 14:21:58 +0900

    refactor: BinaryContent API 반환 타입 DTO에서 Entity로 복구

--- 2-sprint-mission/src/main/java/com/sprint/mission/discodeit/controller/BinaryContentController.java
+++ 2-sprint-mission/src/main/java/com/sprint/mission/discodeit/controller/BinaryContentController.java
@@ -1,20 +1,17 @@
 package com.sprint.mission.discodeit.controller;
 
-import com.sprint.mission.discodeit.dto.BinaryContentDto;
 import com.sprint.mission.discodeit.entity.BinaryContent;
 import com.sprint.mission.discodeit.service.BinaryContentService;
 import jakarta.validation.Valid;
+import java.util.List;
+import java.util.UUID;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.List;
-import java.util.UUID;
 
 @RestController
 @RequestMapping("/api/binaryContents")
@@ -24,21 +21,16 @@ public class BinaryContentController {
   private final BinaryContentService binaryContentService;
 
   @GetMapping("/{binaryContentId}")
-  public ResponseEntity<BinaryContentDto.Response> find(
+  public ResponseEntity<BinaryContent> find(
       @PathVariable UUID binaryContentId) {
     BinaryContent binaryContent = binaryContentService.findById(binaryContentId);
-    return ResponseEntity.ok(BinaryContentDto.Response.of(binaryContent));
+    return ResponseEntity.ok(binaryContent);
   }
 
   @GetMapping
-  public ResponseEntity<List<BinaryContentDto.Response>> findAllByIdIn(
+  public ResponseEntity<List<BinaryContent>> findAllByIdIn(
       @Valid @RequestParam List<UUID> binaryContentIds) {
     List<BinaryContent> binaryContents = binaryContentService.findAllByIdIn(binaryContentIds);
-
-    List<BinaryContentDto.Response> responseList = binaryContents.stream()
-        .map(BinaryContentDto.Response::of)
-        .toList();
-    
-    return ResponseEntity.ok(responseList);
+    return ResponseEntity.ok(binaryContents);
   }
 }
