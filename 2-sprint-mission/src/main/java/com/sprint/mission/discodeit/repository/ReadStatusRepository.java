ba5830f9062263f9301b7d8616d81a2fee7d8ec7
Ksinny <dosly2@naver.com>
2026-04-19 16:30:30 +0900

    feat: JpaRepository 도입 및 File/JCF 구현체 제거

--- 2-sprint-mission/src/main/java/com/sprint/mission/discodeit/repository/ReadStatusRepository.java
+++ 2-sprint-mission/src/main/java/com/sprint/mission/discodeit/repository/ReadStatusRepository.java
@@ -2,19 +2,9 @@ package com.sprint.mission.discodeit.repository;
 
 
 import com.sprint.mission.discodeit.entity.ReadStatus;
-import java.util.List;
-import java.util.Optional;
 import java.util.UUID;
+import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ReadStatusRepository {
+public interface ReadStatusRepository extends JpaRepository<ReadStatus, UUID> {
 
-  ReadStatus save(ReadStatus readStatus);
-
-  Optional<ReadStatus> findById(UUID id);
-
-  List<ReadStatus> findAll();
-
-  boolean existsById(UUID id);
-
-  void deleteById(UUID id);
 }
--- 2-sprint-mission/src/main/java/com/sprint/mission/discodeit/repository/UserRepository.java
+++ 2-sprint-mission/src/main/java/com/sprint/mission/discodeit/repository/UserRepository.java
@@ -1,25 +1,15 @@
 package com.sprint.mission.discodeit.repository;
 
 import com.sprint.mission.discodeit.entity.User;
-import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
+import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface UserRepository {
+public interface UserRepository extends JpaRepository<User, UUID> {
 
-  User save(User user);
+  Optional<User> findByUsername(String userName);
 
-  Optional<User> findById(UUID id);
-
-  Optional<User> findByName(String userName);
-
-  List<User> findAll();
-
-  boolean existsById(UUID id);
-
-  void deleteById(UUID id);
-
-  boolean existsByName(String name);
+  boolean existsByUsername(String name);
 
   boolean existsByEmail(String email);
 }
