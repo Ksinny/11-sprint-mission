533fc13150fa2c529407a094155322238d141dd6
Ksinny <dosly2@naver.com>
2026-06-24 14:57:53 +0900

    feat: 세션 기반 현재 사용자 조회 API 추가

--- 2-sprint-mission/src/main/java/com/sprint/mission/discodeit/controller/AuthController.java
+++ 2-sprint-mission/src/main/java/com/sprint/mission/discodeit/controller/AuthController.java
@@ -1,10 +1,14 @@
 package com.sprint.mission.discodeit.controller;
 
-import com.sprint.mission.discodeit.service.AuthService;
+import com.sprint.mission.discodeit.dto.UserDto;
+import com.sprint.mission.discodeit.security.DiscodeitUserDetails;
+import com.sprint.mission.discodeit.service.UserService;
+import java.util.UUID;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.web.csrf.CsrfToken;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,12 +20,21 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 public class AuthController {
 
-  private final AuthService basicAuthService;
+  private final UserService userService;
 
   @GetMapping("/csrf-token")
   public ResponseEntity<Void> getCsrfToken(CsrfToken csrfToken) {
     String tokenValue = csrfToken.getToken();
     log.debug("CSRF 토큰 요청: {}", tokenValue);
     return ResponseEntity.status(HttpStatus.NON_AUTHORITATIVE_INFORMATION).build();
+  }
+
+  @GetMapping("/me")
+  public ResponseEntity<UserDto.Response> getMe(
+      @AuthenticationPrincipal DiscodeitUserDetails userDetails) {
+    log.debug("내 정보 조회 요청: username={}", userDetails.getUsername());
+    UUID userId = userDetails.getUserDto().id();
+    UserDto.Response userDto = userService.findById(userId);
+    return ResponseEntity.status(HttpStatus.OK).body(userDto);
   }
 }
