9dac2c759c72c5bdbb412dbc608fab201001112c
Ksinny <dosly2@naver.com>
2026-06-24 11:18:03 +0900

    feat: CSRF 토큰 처리 구현

--- 2-sprint-mission/src/main/java/com/sprint/mission/discodeit/controller/AuthController.java
+++ 2-sprint-mission/src/main/java/com/sprint/mission/discodeit/controller/AuthController.java
@@ -6,7 +6,10 @@ import com.sprint.mission.discodeit.service.AuthService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.web.csrf.CsrfToken;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,6 +22,13 @@ import org.springframework.web.bind.annotation.RestController;
 public class AuthController {
 
   private final AuthService basicAuthService;
+
+  @GetMapping("/csrf-token")
+  public ResponseEntity<Void> getCsrfToken(CsrfToken csrfToken) {
+    String tokenValue = csrfToken.getToken();
+    log.debug("CSRF 토큰 요청: {}", tokenValue);
+    return ResponseEntity.status(HttpStatus.NON_AUTHORITATIVE_INFORMATION).build();
+  }
 
   @PostMapping("/login")
   public ResponseEntity<UserDto.Response> login(
