efe34039893d3c73125e3036214877cf4ea148c4
Ksinny <dosly2@naver.com>
2026-06-24 14:43:13 +0900

    refactor: 기존 로그인 구현 제거

--- 2-sprint-mission/src/main/java/com/sprint/mission/discodeit/controller/AuthController.java
+++ 2-sprint-mission/src/main/java/com/sprint/mission/discodeit/controller/AuthController.java
@@ -1,17 +1,12 @@
 package com.sprint.mission.discodeit.controller;
 
-import com.sprint.mission.discodeit.dto.AuthDto;
-import com.sprint.mission.discodeit.dto.UserDto;
 import com.sprint.mission.discodeit.service.AuthService;
-import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.web.csrf.CsrfToken;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -29,16 +24,4 @@ public class AuthController {
     log.debug("CSRF 토큰 요청: {}", tokenValue);
     return ResponseEntity.status(HttpStatus.NON_AUTHORITATIVE_INFORMATION).build();
   }
-
-  @PostMapping("/login")
-  public ResponseEntity<UserDto.Response> login(
-      @Valid @RequestBody AuthDto.LoginRequest request
-  ) {
-    log.info("로그인 요청: username={}", request.username());
-    UserDto.Response response = basicAuthService.login(request);
-
-    log.debug("로그인 응답: {}", response);
-    return ResponseEntity.ok(response);
-  }
-
 }
--- 2-sprint-mission/src/main/java/com/sprint/mission/discodeit/service/basic/BasicAuthService.java
+++ 2-sprint-mission/src/main/java/com/sprint/mission/discodeit/service/basic/BasicAuthService.java
@@ -1,11 +1,5 @@
 package com.sprint.mission.discodeit.service.basic;
 
-import com.sprint.mission.discodeit.dto.AuthDto;
-import com.sprint.mission.discodeit.dto.UserDto;
-import com.sprint.mission.discodeit.entity.User;
-import com.sprint.mission.discodeit.exception.user.InvalidCredentialsException;
-import com.sprint.mission.discodeit.mapper.UserMapper;
-import com.sprint.mission.discodeit.repository.UserRepository;
 import com.sprint.mission.discodeit.service.AuthService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -18,21 +12,4 @@ import org.springframework.transaction.annotation.Transactional;
 @Transactional(readOnly = true)
 public class BasicAuthService implements AuthService {
 
-  private final UserRepository userRepository;
-  private final UserMapper userMapper;
-
-  @Override
-  public UserDto.Response login(AuthDto.LoginRequest request) {
-    log.debug("로그인 시작: username={}", request.username());
-    User user = userRepository.findByUsername(request.username())
-        .orElseThrow(InvalidCredentialsException::wrongPassword);
-
-    // 비밀번호 검증
-    if (!user.matchesPassword(request.password())) {
-      throw InvalidCredentialsException.wrongPassword();
-    }
-
-    log.info("로그인 성공: userId={}, username={}", user.getId(), user.getUsername());
-    return userMapper.toDto(user);
-  }
 }
