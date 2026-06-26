8ffd534d5296f3de7486776076b49229c7f71f38
Ksinny <dosly2@naver.com>
2026-06-26 10:47:46 +0900

    feat: URL 기반 인가 설정

--- 2-sprint-mission/src/main/java/com/sprint/mission/discodeit/config/SecurityConfig.java
+++ 2-sprint-mission/src/main/java/com/sprint/mission/discodeit/config/SecurityConfig.java
@@ -5,14 +5,19 @@ import com.sprint.mission.discodeit.security.LoginSuccessHandler;
 import com.sprint.mission.discodeit.security.SpaCsrfTokenRequestHandler;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.logout.HttpStatusReturningLogoutSuccessHandler;
 import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
+import org.springframework.security.web.servlet.util.matcher.PathPatternRequestMatcher;
+import org.springframework.security.web.util.matcher.NegatedRequestMatcher;
 
+@EnableMethodSecurity
 @Configuration
 public class SecurityConfig {
 
@@ -34,6 +39,16 @@ public class SecurityConfig {
             .logoutUrl("/api/auth/logout")
             .logoutSuccessHandler(
                 new HttpStatusReturningLogoutSuccessHandler(HttpStatus.NO_CONTENT))
+        )
+        .authorizeHttpRequests(auth -> auth
+            .requestMatchers(HttpMethod.GET, "/api/auth/csrf-token").permitAll()
+            .requestMatchers(HttpMethod.POST, "/api/users").permitAll()
+            .requestMatchers(HttpMethod.POST, "/api/auth/login").permitAll()
+            .requestMatchers(HttpMethod.POST, "/api/auth/logout").permitAll()
+            .requestMatchers(new NegatedRequestMatcher(
+                PathPatternRequestMatcher.withDefaults().matcher("/api/**")
+            )).permitAll()
+            .anyRequest().authenticated()
         );
     return http.build();
   }
