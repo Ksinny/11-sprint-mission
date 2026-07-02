16da88a9c7d68ec93776693a9f1e71c999b2082a
Ksinny <dosly2@naver.com>
2026-07-02 15:52:41 +0900

    feat: JwtRegistry 기반 online 여부 확인 복구

--- 2-sprint-mission/src/main/java/com/sprint/mission/discodeit/mapper/UserMapper.java
+++ 2-sprint-mission/src/main/java/com/sprint/mission/discodeit/mapper/UserMapper.java
@@ -2,13 +2,18 @@ package com.sprint.mission.discodeit.mapper;
 
 import com.sprint.mission.discodeit.dto.UserDto;
 import com.sprint.mission.discodeit.entity.User;
+import com.sprint.mission.discodeit.security.jwt.JwtRegistry;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
+import org.springframework.beans.factory.annotation.Autowired;
 
 @Mapper(componentModel = "spring", uses = {BinaryContentMapper.class})
 public abstract class UserMapper {
 
-  // TODO: JwtRegistry 기반 교체 예정
-  @Mapping(target = "online", constant = "false")
+  @Autowired
+  protected JwtRegistry jwtRegistry;
+
+  @Mapping(target = "online",
+      expression = "java(jwtRegistry.hasActiveJwtInformationByUserId(user.getId()))")
   public abstract UserDto.Response toDto(User user);
 }
