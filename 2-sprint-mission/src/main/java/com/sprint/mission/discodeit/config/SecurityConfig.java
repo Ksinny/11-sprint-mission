9b4a5309c0c2ab69d5a3c4bf9f64e7dd5800dbba
Ksinny <dosly2@naver.com>
2026-07-02 15:00:32 +0900

    feat: refresh token으로 access token 재발급 API 구현

--- 2-sprint-mission/src/main/java/com/sprint/mission/discodeit/config/SecurityConfig.java
+++ 2-sprint-mission/src/main/java/com/sprint/mission/discodeit/config/SecurityConfig.java
@@ -56,6 +56,7 @@ public class SecurityConfig {
             .requestMatchers(HttpMethod.POST, "/api/users").permitAll()
             .requestMatchers(HttpMethod.POST, "/api/auth/login").permitAll()
             .requestMatchers(HttpMethod.POST, "/api/auth/logout").permitAll()
+            .requestMatchers(HttpMethod.POST, "/api/auth/refresh").permitAll()
             .requestMatchers(new NegatedRequestMatcher(
                 PathPatternRequestMatcher.withDefaults().matcher("/api/**")
             )).permitAll()
