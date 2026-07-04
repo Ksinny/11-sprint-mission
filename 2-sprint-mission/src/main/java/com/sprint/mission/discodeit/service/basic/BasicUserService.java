f1686d61cd47cb8a2f65e2cba90b04ef3c4d5075
Ksinny <dosly2@naver.com>
2026-07-04 19:30:28 +0900

    refactor: 사용자 수정/삭제 권한에 ADMIN 접근 허용 추가

--- 2-sprint-mission/src/main/java/com/sprint/mission/discodeit/service/basic/BasicUserService.java
+++ 2-sprint-mission/src/main/java/com/sprint/mission/discodeit/service/basic/BasicUserService.java
@@ -97,7 +97,7 @@ public class BasicUserService implements UserService {
 
   @Override
   @Transactional
-  @PreAuthorize("#id == principal.userDto.id")
+  @PreAuthorize("hasRole('ADMIN') or #id == principal.userDto.id")
   public UserDto.Response update(UUID id, UserDto.UpdateRequest request,
       BinaryContentDto.CreateRequest profileImageRequest) {
     log.debug("사용자 업데이트 시작: id={}", id);
@@ -154,7 +154,7 @@ public class BasicUserService implements UserService {
 
   @Override
   @Transactional
-  @PreAuthorize("#id == principal.userDto.id")
+  @PreAuthorize("hasRole('ADMIN') or #id == principal.userDto.id")
   public void delete(UUID id) {
     log.debug("사용자 삭제 시작: id={}", id);
 
