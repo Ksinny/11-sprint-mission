052d6834786ea49ea1adc6be17ddf1069a517220
Ksinny <dosly2@naver.com>
2026-07-18 17:34:57 +0900

    feat: S3 바이너리 데이터 저장에 자동 retry 적용

--- 2-sprint-mission/src/main/java/com/sprint/mission/discodeit/storage/s3/S3BinaryContentStorage.java
+++ 2-sprint-mission/src/main/java/com/sprint/mission/discodeit/storage/s3/S3BinaryContentStorage.java
@@ -17,6 +17,8 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.retry.annotation.Backoff;
+import org.springframework.retry.annotation.Retryable;
 import org.springframework.stereotype.Component;
 import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
 import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
@@ -77,6 +79,11 @@ public class S3BinaryContentStorage implements BinaryContentStorage {
     }
   }
 
+  @Retryable(
+      retryFor = StorageOperationException.class,
+      maxAttempts = 3,
+      backoff = @Backoff(delay = 1000, multiplier = 2)
+  )
   @Override
   public UUID put(UUID binaryContentId, byte[] bytes) {
     String key = binaryContentId.toString();
